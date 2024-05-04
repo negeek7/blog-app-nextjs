9fe55a6a41025a5d37549ee852ab817c306e57c0
Harshiiet Negi <negeek7@gmail.com>
2024-05-05 03:40:39 +0530

    some styling

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,16 +9,20 @@ export default function Home() {
   useEffect(() => {
       fetch('/api/blogs')
       .then(res => res.json())
-      .then(data => setBlogData(JSON.parse(data)))
+      .then(data => setBlogData(data))
   }, []);
 
   console.log(blogData, "BLOG DATA")
 
   return (
     <div className='min-h-screen'>
+      <div className="p-6 flex flex-col justify-center items-center">
+        <h1>The Blog Room</h1>
+        <p className="mt-4 max-w-screen-md text-center text-wrap border-2 border-purple-600">The Blog Room: Your Gateway to Insightful Articles and Engaging Stories. Explore a diverse collection of topics, from technology and lifestyle to culture and beyond. Dive into thought-provoking content curated to inform, inspire, and entertain.</p>
+      </div>
       <div className='border-2 border-yellow-400'>
         {
-          blogData.map(blog => (
+          blogData?.map(blog => (
             <>
               <BlogTile blog={blog} />
             </>
--- src/components/BlogTile.jsx
+++ src/components/BlogTile.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function BlogTile({blog}) {
   return (
     <div className="border-2 border-pink-400">
-
-
-
+        <h3>{blog.title}</h3>
+        <p>{blog.summary}</p>
+        <p>{blog.content}</p>
     </div>
   )
 }
