07c0bb2a75c634b0b5ab39bf4e9a62c235353b38
Harshiiet Negi <negeek7@gmail.com>
2024-05-05 19:27:44 +0530

    some styling

--- src/components/BlogTile.jsx
+++ src/components/BlogTile.jsx
@@ -14,12 +14,17 @@ function BlogTile({blog}) {
 
   return (
     <>
-        <div className="border-2 border-pink-400 cursor-pointer max-h-80">
     <Link href={`/blog/${blog.id}`}>
-            <h3 className='m-0 text-center p-8 bg-slate-500 truncate'>{blog.title}</h3>
-            <p>{getBlogTileContent(blog.content)}</p>
-    </Link>
+        <div className="cursor-pointer shadow-[0px_0px_12px_0px_#44337a] rounded-xl hover:scale-105 transition-all ease-in-out duration-500">
+            <div className='text-center p-6 rounded-tl-xl'>
+                <h2 className='truncate'>{blog.title}</h2>
+            </div>
+            <div className="p-4 rounded-br-xl">
+                <p className="text-gray-300">{getBlogTileContent(blog.content)}</p>
+            </div>
+            
         </div>
+    </Link>
     </>
   )
 }
