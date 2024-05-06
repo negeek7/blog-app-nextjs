908d9352997f25de868bdcb8b4d04eb30a64afce
Harshiiet Negi <negeek7@gmail.com>
2024-05-06 07:11:51 +0530

    go back from blog post

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,5 +1,7 @@
 'use client'
+import { ArrowFatLeft } from '@phosphor-icons/react/dist/ssr'
 import { usePathname } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
 function BlogPage() {
@@ -9,6 +11,7 @@ function BlogPage() {
 
   const path = usePathname()
   const blogid = path.split('/')[2]
+  const router = useRouter()
 
   useEffect(() => {
     setLoading(true)
@@ -23,13 +26,14 @@ function BlogPage() {
     {
       loading ? <p>Loading....</p>
       :
-      <div className='text-white p-8'>
-        <div className="p-8 h-80 flex flex-col items-center justify-center gap-10 shadow-[0px_0px_82px_5px_#44337a] rounded-tr-xl
+      <div className='relative text-white p-8'>
+        <div className=" relative p-8 h-80 flex flex-col items-center justify-center gap-10 shadow-[0px_0px_82px_5px_#44337a] rounded-tr-xl
         rounded-bl-xl">
+          <ArrowFatLeft className="absolute left-2 top-2 sm:left-6 sm:top-6 cursor-pointer text-lg sm:text-4xl" onClick={() => router.back()}/>
           <h1>{blog?.title}</h1>
           <span>{blog?.summary}</span>
         </div>
-        <div className="mt-14">
+        <div className="mt-14 leading-8">
           <p>{blog?.content}</p>
         </div>
       </div>
